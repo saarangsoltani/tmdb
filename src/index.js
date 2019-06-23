421534ca2ac0e3f6f980c8298faa6b2542e75a6b
saarangslt <sahandfx@gmail.com>
2019-06-23 21:13:09 +0100

    Add a basic redux store and reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "./store";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<h1>Its a wilderness here</h1>, document.getElementById('root'));
+
+const targetEl = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history} />
+    <h1>Its still a wildreness here</h1>
+  </Provider>,
+  targetEl
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
